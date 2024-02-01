a5bd655ca3a818819c062a1c5b1f4a67e4d9089d
Chitra <chitradvij@gmail.com>
2024-02-02 00:11:16 +0530

    new branch

--- src/HelloWorldPrinter.java
+++ src/HelloWorldPrinter.java
@@ -5,5 +5,8 @@ public class HelloWorldPrinter implements Runnable {
         //What work we want to do in a separate thread, that will come here.
         System.out.println("Hello World. THREAD: " +
                 Thread.currentThread().getName());
+        
+        //Change demo github
+        System.out.println("demo for git");
     }
 }
