e5fc1e09c54306a496a5ac06207427c501e883ac
lokesh-purplle <129851301+lokesh-purplle@users.noreply.github.com>
2024-02-01 23:08:18 +0530

    Update HelloWorldPrinter.java

--- src/HelloWorldPrinter.java
+++ src/HelloWorldPrinter.java
@@ -5,5 +5,6 @@ public class HelloWorldPrinter implements Runnable {
         //What work we want to do in a separate thread, that will come here.
         System.out.println("Hello World. THREAD: " +
                 Thread.currentThread().getName());
+        System.out.println("lokesh's commit ");
     }
 }
