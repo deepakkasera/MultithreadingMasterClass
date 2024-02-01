76cdb1bacb636edc537ae47fa3417d1c72556509
hsai2696 <harshasai123456@gmail.com>
2024-02-01 23:10:51 +0530

    Update RandomNumberGenerator.java

--- src/RandomNumberGenerator.java
+++ src/RandomNumberGenerator.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 import java.util.concurrent.Callable;
 
-public class RandomNumberGenerator implements Callable {
+public class RandomNumberGenerator implements Callable<Integer> {
     @Override
     public Integer call() throws Exception {
         Random random = new Random();
